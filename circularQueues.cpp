96aea10f9f40157405945bb71e0187ee59168059
rismamutia <rismamutiadewi89@gmail.com>
2025-06-12 14:44:17 +0700

    membuat inputan front dan rear

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -4,4 +4,16 @@ using namespace std;
 class Queues
 {
     private:
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
+
+    public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    
 }
